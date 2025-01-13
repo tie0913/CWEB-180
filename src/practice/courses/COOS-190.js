1922705b4a27800d7b4d4fa30f4658dbc5fde4c6
Wang <wang9431@saskpolytech.ca>
2025-01-13 15:09:20 -0600

    add index providing course and chapter selection

---
+++ src/practice/courses/COOS-190.js
@@ -0,0 +1,50 @@
+export const TITLE = "COOS-190"
+export const chapter_list  = [{
+    "chapter_name" : "chapter_1",
+    "question_list" : [{
+        "text": "You are setting up a secure website for your online store. You want to ensure that all data transmitted between your website and your customers is encrypted.\nWhich of the following steps is essential for you to achieve this?\n",
+        "options": [
+            {
+                "text": "Install a web analytics tool.",
+                "correct": "0"
+            },
+            {
+                "text": "Obtain and install a digital certificate.",
+                "correct": "1"
+            },
+            {
+                "text": "Increase your website's bandwidth.",
+                "correct": "0"
+            },
+            {
+                "text": "Implement a CAPTCHA system on your website.",
+                "correct": "0"
+            }
+        ],
+        "type": "single"
+    }]
+},{
+    "chapter_name":"chapter_2",
+    "question_list":[ {
+        "text": "What is established between a server and a client after successful authentication with TLS?\n",
+        "options": [
+            {
+                "text": "A direct physical connection",
+                "correct": "0"
+            },
+            {
+                "text": "A public network",
+                "correct": "0"
+            },
+            {
+                "text": "A data compression protocol",
+                "correct": "0"
+            },
+            {
+                "text": "An encrypted tunnel",
+                "correct": "1"
+            }
+        ],
+        "type": "single"
+    }]
+}]
---
+++ src/practice/index.js
@@ -0,0 +1,50 @@
+"use strict"
+
+const courseList =[{
+    "name":"COOS-190",
+    "chapter_list":["chapter_1", "chapter_2"]
+}]
+
+let courseSelect = document.getElementById("course_select")
+for(let i = 0, len = courseList.length; i < len; i++){
+    let option = document.createElement("option")
+    let courseObj = courseList[i]
+    option.value = courseObj.name
+    option.innerText = courseObj.name
+    courseSelect.append(option)
+}
+
+function getChapterListByCourseName(courseName){
+    for(let i = 0, len = courseList.length; i < len; i++){
+        let courseObj = courseList[i]
+        if(courseObj.name === courseName){
+            return courseObj.chapter_list
+        }
+    }
+    return undefined
+}
+
+function emptyChapterSelect(){
+    let chapterSelect = document.getElementById("chapter_select")
+    for(let i = chapterSelect.options.length - 1; i > 0; i--){
+        chapterSelect.remove(i)
+    }
+}
+
+function courseIsSelected(){
+    let selectedValue = courseSelect.options[courseSelect.selectedIndex].value
+    let chapterList = getChapterListByCourseName(selectedValue)
+    emptyChapterSelect()
+    if(selectedValue !== ""){
+        let chapterSelect = document.getElementById("chapter_select")
+        for(let i = 0, len = chapterList.length; i < len; i++){
+            let chapterName = chapterList[i]
+            let option = document.createElement("option")
+            option.value = chapterName
+            option.innerText = chapterName
+            chapterSelect.append(option)
+        }
+    }
+}
+
+courseSelect.addEventListener("change", courseIsSelected)
